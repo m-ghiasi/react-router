ac0b0950cb1b8a9688f086dada573d9a316b30b9
m-ghiasi <mahdiye71gh@gmail.com>
2025-01-12 13:46:58 +0330

    loading

--- src/components/GetPeople/index.jsx
+++ src/components/GetPeople/index.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
+import { TailSpin } from "react-loader-spinner";
 
 export default function GetPeople() {
   const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const getData = async () => {
     try {
+      setLoading(true);
       const res = await fetch("https://reqres.in/api/users?page=1");
       const users = await res.json();
       setMembers(users.data);
       console.log(users.data);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -19,27 +24,30 @@ export default function GetPeople() {
     getData();
   }, []);
 
-  return <div className="flex flex-col"> 
-  
-    <ul>
-    {members.map((member, index)=>(
-    
-      <NavLink key={index} to={`/people/${member.id}`}>
-      <li className="flex p-4 gap-4 items-center rounded-2xl  hover:bg-slate-400">
-        <img src={member.avatar} alt=""
-        width={64}
-        height={64}
-        className="rounded-full" />
-      </li>
-      <span>{member.first_name} {member.last_name}</span>
-      
-      
-      </NavLink>
-    
-    
-
-  ))}
-  </ul>
-
-  </div>;
+  return (
+    <div className="flex flex-col">
+      <ul>
+        {loading ? (
+          <TailSpin  height="24" width="24"  />
+        ) : (
+          members.map((member, index) => (
+            <NavLink key={index} to={`/people/${member.id}`}>
+              <li className="flex p-4 gap-4 items-center rounded-2xl  hover:bg-slate-400">
+                <img
+                  src={member.avatar}
+                  alt=""
+                  width={64}
+                  height={64}
+                  className="rounded-full"
+                />
+              </li>
+              <span>
+                {member.first_name} {member.last_name}
+              </span>
+            </NavLink>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 }
